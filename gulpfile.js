01dc5e9da265ec3a4a9a90825ba304182b1c9e96
Michael West <michaelwest@COH000014.local>
2016-06-27 14:07:37 +0100

    restructured gulp tasks and created dist/src workflow

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,36 @@ var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
 
-gulp.task('workflow', function () {
-	gulp.src('stylesheets/scss/*.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(cssnano())
-		.pipe(sourcemaps.write('./'))
-		
-	.pipe(gulp.dest('stylesheets/css/'))
+//Convert scss to css
+gulp.task('sass', function () {
+  gulp.src('src/stylesheets/scss/*.scss')
+	    .pipe(sass())
+	    .pipe(gulp.dest('src/stylesheets/css/'));
 });
-
+//Minify css
+gulp.task('compresscss', function() {
+    gulp.src('src/stylesheets/css/*.css')
+	    .pipe(cssnano())
+	    .pipe(rename({
+	  		suffix: '.min'
+		}))
+	    .pipe(gulp.dest('dist/stylesheets/css/'));
+});
+//Minify js
+gulp.task('compressjs', function () {
+  gulp.src('src/js/*.js')
+	    .pipe(uglify())
+	    .pipe(rename({
+	      suffix: '.min'
+	    }))
+    .pipe(gulp.dest('dist/js/'));
+});
+//Watch scss, css and js files for changes
 gulp.task('default', function () {
-	gulp.watch('stylesheets/scss/*.scss', ['workflow']);
+	gulp.watch(['src/stylesheets/scss/*.scss'], ['sass']);
+	gulp.watch(['src/stylesheets/css/*.css'], ['compresscss']);
+	gulp.watch(['src/js/*.js'], ['compressjs']);
 });

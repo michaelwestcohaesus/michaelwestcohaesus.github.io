26b7e31759053f1f8988a9d44182dde897f8d0a5
Michael West <michaelwest@COH000014.home>
2016-07-02 16:49:36 +0100

    disableTextArea.js reformat

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,17 +1,11 @@
 function toggleCheckbox(){
-var checkbox = document.getElementById("checkboxOther");
-if(checkbox.checked)
-  document.getElementById("otherQuestion4").disabled = false;
-else
-  document.getElementById("otherQuestion4").disabled = true;    
+	var checkbox = document.getElementById("checkboxOther");
+	checkbox.checked?document.getElementById("otherQuestion4").disabled = false: document.getElementById("otherQuestion4").disabled = true;
 }
 
 function toggleRadio(){
-var radiobutton = document.getElementById("radioOption11");
-if(radiobutton.checked)
-  document.getElementById("otherQuestion5").disabled = false;
-else
-  document.getElementById("otherQuestion5").disabled = true;    
+	var radiobutton = document.getElementById("radioOption11");
+	radiobutton.checked?document.getElementById("otherQuestion5").disabled = false: document.getElementById("otherQuestion5").disabled = true;
 }
 $.fn.serializeObject = function()
 {
